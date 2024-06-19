500cd3c0647a6149c8e98b40db04cdaab444ed06
dionebraga <dionebraga.work@gmail.com>
2024-06-19 19:13:00 -0300

    Calculadora V.1

---
+++ Calculadora.py
@@ -0,0 +1,24 @@
+def calculadora(operacao, num1, num2):
+    match operacao:
+        case "adicao":
+            return num1 + num2
+        case "subtracao":
+            return num1 - num2
+        case "multiplicacao":
+            return num1 * num2
+        case "divisao":
+            return num1 / num2
+        case _:
+            return "Operação inválida"
+
+def main():
+    print("Escolha a operação: adicao, subtracao, multiplicacao, divisao")
+    operacao = input("Digite a operação: ").strip().lower()
+    num1 = float(input("Digite o primeiro número: "))
+    num2 = float(input("Digite o segundo número: "))
+
+    resultado = calculadora(operacao, num1, num2)
+    print(f"O resultado da {operacao} é: {resultado}")
+
+if __name__ == "_main_":
+    main()
